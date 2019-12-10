d77fdce15c513e5401afa24ae7dd254fc25eaf9c
Miral Doshi <miraldoshi@zipcodersMBP41.localdomain>
2019-12-10 14:31:41 -0500

    done

---
+++ answers/exercise1.sql
@@ -0,0 +1,6 @@
+  SELECT *
+    FROM Enrolments
+    LEFT JOIN Students
+    ON Enrolments.StudentID  = Students.StudentID;
+
+
---
+++ answers/exercise6.sql
@@ -0,0 +1,5 @@
+SELECT COUNT(StudentID), Country
+FROM Students
+GROUP BY Country
+ORDER BY COUNT(StudentID) DESC
+WHENEVER COUNT(StudentID) > 10; ;
